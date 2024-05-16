fc249a2bb8774863630aee74c9b2641404be6286
khadmalek <k.abdelmalek@live.fr>
2024-05-16 19:04:20 +0200

    add project and advanced

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
+class Square:
+    """ Square class that define a square with is size"""
+    def __init__(self, size):
+        """square size private instance attribute"""
+        self.__size = size
