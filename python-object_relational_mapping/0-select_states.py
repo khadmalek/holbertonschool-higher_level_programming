685d748c04f98245625c6468ff1af917178bfc1f
khadmalek <k.abdelmalek@live.fr>
2024-06-21 22:00:43 +0200

    grind code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""Module listing all states from the database"""
-
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    # Connect to the database
-    db = MySQLdb.connect(
-          host="localhost",
-          port=3306,
-          user=argv[1],
-          passwd=argv[2],
-          db=argv[3],
-          )
+# Check if all required arguments are provided
+if len(sys.argv) != 4:
+    print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    sys.exit(1)
 
-    # Create a cursor object to interact with the database
-    cur = db.cursor()
+# Retrieve command-line arguments
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
 
-    # Execute the SQL query to select all states ordered by their ID
-    cur.execute("""SELECT * FROM states ORDER BY id""")
+# print("Connecting to MySQL with user={}, password={}, database={}"
+# .format(username, password, database))
 
-    # Fetch all the rows returned by the query
-    query_rows = cur.fetchall()
+# Connect to MySQL
+db = MySQLdb.connect(host="localhost", user=username,
+                     passwd=password, db=database)
 
-    # Print each row
-    for row in query_rows:
-        print(row)
 
-    # Close the cursor and connection
-    cur.close()
-    db.close()
-    
+# Further logic to execute queries can be added here
+cursor = db.cursor()
+
+# Example query to fetch and print all states
+cursor.execute("SELECT * FROM states ORDER BY id")
+results = cursor.fetchall()
+for row in results:
+    print(row)
+
+# Close the cursor and database connection
+cursor.close()
+db.close()
