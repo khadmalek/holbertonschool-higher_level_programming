6fbb9fc09c721cac0742638f00b845af948d574a
khadmalek <k.abdelmalek@live.fr>
2024-06-21 12:18:50 +0200

    script that removes all records with a score <= 5 in the table second_table of the database

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Tâche 13 : Supprimer les enregistrements avec score <= 5 dans la table second_table
+-- Script SQL pour supprimer les enregistrements avec score <= 5
+
+DELETE FROM second_table WHERE score <= 5;
