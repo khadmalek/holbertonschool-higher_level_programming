678ef86564f5d264a9b028745a1601c0d003cb10
khadmalek <k.abdelmalek@live.fr>
2024-06-21 11:58:29 +0200

    script that displays the number of records with id = 89

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Tâche 8 : Afficher le nombre d'enregistrements avec id = 89
+-- Script SQL pour compter et afficher le nombre de lignes où id = 89
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
