2329f5691f9e9fae69362884e042d72f529fa24d
khadmalek <k.abdelmalek@live.fr>
2024-06-21 12:21:36 +0200

    script that computes the score average of all records in the table second_table of the database

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Tâche 14 : Calculer la moyenne des scores dans la table second_table
+-- Script SQL pour calculer la moyenne des scores et renommer la colonne résultante en 'average'
+
+SELECT AVG(score) AS average FROM second_table;
