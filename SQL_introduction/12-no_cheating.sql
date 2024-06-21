7ed0c60f80ab52eabc8dea5ea7a909baca5818b0
khadmalek <k.abdelmalek@live.fr>
2024-06-21 12:16:40 +0200

    script that updates the score of Bob to 10 in the table second_table

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Tâche 12 : Mettre à jour le score de Bob dans la table second_table
+-- Script SQL pour mettre à jour le score de l'entrée avec le nom "Bob"
+
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
