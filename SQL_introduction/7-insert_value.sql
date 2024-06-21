98fd4ff210f7b1e68cdd3364812974de70355aac
khadmalek <k.abdelmalek@live.fr>
2024-06-21 11:56:20 +0200

    script that inserts a new row in the table first_table

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Tâche 7 : Insérer une nouvelle ligne dans la table first_table
+-- Script SQL pour insérer une nouvelle ligne avec id=89 et name='Best School'
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
