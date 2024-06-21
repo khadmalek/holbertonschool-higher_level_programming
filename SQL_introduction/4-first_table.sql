9cae329a02f16e60f1ee25ae4808c8443f9555bd
khadmalek <k.abdelmalek@live.fr>
2024-06-21 11:45:29 +0200

    script that creates a table called first_table in the current database

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Tâche 4 : Créer la table first_table
+-- Script SQL pour créer la table first_table si elle n'existe pas
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
