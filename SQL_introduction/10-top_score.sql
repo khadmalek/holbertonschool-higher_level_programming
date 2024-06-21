a7873f467d2bcf7e868ad694001789ac11cf15f6
khadmalek <k.abdelmalek@live.fr>
2024-06-21 12:07:47 +0200

    script that lists all records of the table second_table of the database

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Tâche 10 : Lister tous les enregistrements de la table second_table
+-- Script SQL pour lister tous les enregistrements avec score et name, ordonnés par score décroissant
+
+SELECT score, name FROM second_table ORDER BY score DESC;
