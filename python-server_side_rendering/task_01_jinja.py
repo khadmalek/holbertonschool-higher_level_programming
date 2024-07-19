3e73b331bacd30682815e88656e0e76efcf6376e
khadmalek <k.abdelmalek@live.fr>
2024-07-19 14:09:04 +0200

    add item html and json

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,3 +1,5 @@
+import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -13,6 +15,15 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    # Lire les données du fichier JSON
+    with open('items.json', 'r') as file:
+        data = json.load(file)
+    
+    # Rendre le template avec la liste d'items
+    return render_template('items.html', items=data.get('items', []))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
