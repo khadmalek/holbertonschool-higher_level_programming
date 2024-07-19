6f6d724be7abccc5c3d525323f18b8310ff9f54b
khadmalek <k.abdelmalek@live.fr>
2024-07-19 13:57:42 +0200

    add directory templates with files

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,13 +1,14 @@
--- Script to create database hbtn_0d_2 and user user_0d_2 with SELECT privilege
+-- Script pour créer la base de données hbtn_0d_2 et l'utilisateur user_0d_2 avec le privilège SELECT
 
--- Create or ensure the existence of database hbtn_0d_2
+-- Créer ou s'assurer de l'existence de la base de données hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Create or ensure the existence of user user_0d_2 and set the password
+-- Créer ou s'assurer de l'existence de l'utilisateur user_0d_2 et définir le mot de passe
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grant SELECT privilege on database hbtn_0d_2 to user_0d_2
+-- Accorder le privilège SELECT sur la base de données hbtn_0d_2 à user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
--- Flush privileges to apply changes
+-- Appliquer les changements
 FLUSH PRIVILEGES;
+
